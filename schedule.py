3ed4f10631370a0268b5b2c8e75605132a277e9d
Cr1m50nr34p3r <akshatgarg789@gmail.com>
2022-03-11 22:59:58 +0530

    Added Upload feature

--- schedule.py
+++ schedule.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
-############################################################
-###          _              _       _                    ###
-### ___  ___| |__   ___  __| |_   _| | ___   _ __  _   _ ###
-###/ __|/ __| '_ \ / _ \/ _` | | | | |/ _ \ | '_ \| | | |###
-###\__ \ (__| | | |  __/ (_| | |_| | |  __/_| |_) | |_| |###
-###|___/\___|_| |_|\___|\__,_|\__,_|_|\___(_) .__/ \__, |###
-###                                         |_|    |___/ ###
-############################################################
-### Imports
+##########################################################
+#           _              _       _                     #
+#  ___  ___| |__   ___  __| |_   _| | ___   _ __  _   _  #
+# / __|/ __| '_ \ / _ \/ _` | | | | |/ _ \ | '_ \| | | | #
+# \__ \ (__| | | |  __/ (_| | |_| | |  __/_| |_) | |_| | #
+# |___/\___|_| |_|\___|\__,_|\__,_|_|\___(_) .__/ \__, | #
+#                                          |_|    |___/  #
+##########################################################
+# Imports
 from __future__ import print_function
 import datetime
 import pickle
@@ -18,32 +18,83 @@ from google.auth.transport.requests import Request
 import google.auth.exceptions
 import argparse
 import platform
+import sys
 from rich import pretty, print
+import shutil
 
 pretty.install()
-### Variables
+# Variables
 system = platform.system()
 # If modifying these scopes, delete the file /etc/1337/Analyse/token.pickle.
-SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/calendar/"]
+# TIMEZONE = 'India/Kolkata'
+TIMEZONE = 'UTC'
+dGMT = datetime.timedelta(hours=-5, minutes=-30)
 # edit and fill in the calendar ids which can be found in google calendar settings under the settings for specefic label
 # Use format "{label}": "{cal_id}"
 cal_ids = {
     "REGULAR": "akshatgarg789@gmail.com",
+    "STUDY": "tqnor6go6aafauutj6r5c7jqn4@group.calendar.google.com",
+    "REST": "to7hn4rd6amosc2jd317n26kbg@group.calendar.google.com",
+    "HW": "2k7j09evrdkmmoqf231nurgjho@group.calendar.google.com",
+    "EXTRA": "t62kl99bos1gpgq2vs03bu3m80@group.calendar.google.com",
+    "CLASSES": "hs2frss259nb72ho1bjtut8j6o@group.calendar.google.com",
 }
-### Parser
+# Parser
 parser = argparse.ArgumentParser(description='Import google calendar events to doom emacs')
 parser.add_argument('-n',  type=int, required=False,  default=0,  help="day for example 1 = tomorrow default is 0 i.e. today")
-parser.add_argument('--md', type=bool, required=False, default=True, help="save output in markdown")
+parser.add_argument('--md', action='store_true', required=False, help="save/upload output in markdown file")
+parser.add_argument('-u', action='store_true', required=False, help="wheather to upload the file or write to it")
 args = parser.parse_args()
 day_num = args.n
 is_md = args.md
+is_upload = args.u
 
 
-def main():
+def print_center_text(text: str):
+    if system == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+    center_line = int(shutil.get_terminal_size().lines/2-text.count('\n')+1)
+    print('\n'*center_line)
+    ltext = text.splitlines()
+    for line in ltext:
+        s = line.center(shutil.get_terminal_size().columns)
+        print(s)
+    print('\n'*center_line)
+    if system == "Windows":
+        while True:
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                break
+
+
+def get_file():
+    if system == "Windows":
+        logs_dir = os.getenv('USERPROFILE').replace('\\',  '/')
+        +"/Desktop/.dlogs/.Schedule/"
+    else:
+        logs_dir = os.getenv('HOME')+"/.dlogs/.Schedule/"
+    now_dt = datetime.datetime.now()
+    date = (
+        datetime.datetime(
+            year=now_dt.year, month=now_dt.month, day=now_dt.day + day_num
+        )
+    )
+
+    def_date = str(round(int(date.strftime("%Y")),  -1)) + "s/" + date.strftime("%Y") + "/" + date.strftime("%b")
+    if not os.path.exists(logs_dir+def_date):
+        os.makedirs(logs_dir+def_date)
+    sch = logs_dir+def_date + "/" + date.strftime('%d-%m-%Y')
+    return sch
+
+
+def init_api():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
-    events = []
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -53,7 +104,7 @@ def main():
             with open("token.pickle", "rb") as token:
                 creds = pickle.load(token)
         except google.auth.exceptions.RefreshError:
-            print("token.pickle expired creating new one ....")
+            print_center_text("token.pickle expired creating new one ....")
             pass
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -67,8 +118,12 @@ def main():
             pickle.dump(creds, token)
 
     service = build("calendar", "v3", credentials=creds)
+    return service
 
-    # Call the Calendar API
+
+def get_events():
+    events = []
+    # Set up Time variables
     now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
     now_dt = datetime.datetime.now()
     time_min = (
@@ -83,6 +138,7 @@ def main():
         ).isoformat()
         + "Z"
     )
+    service = init_api()
     for cal_id in cal_ids:
 
         cal = cal_ids.get(cal_id.upper())
@@ -99,11 +155,6 @@ def main():
             .execute()
         )
         events += events_result.get("items", [])
-    return events
-
-
-def init_db():
-    events = main()
     events_db = []
     for event in events:
         start = (
@@ -127,52 +178,127 @@ def init_db():
     return events_db
 
 
-def md():
-    if system == "Windows":
-        logs_dir = os.getenv('USERPROFILE').replace('\\',  '/')
-        +"/Desktop/.dlogs/.Schedule/"
-    else:
-        logs_dir = os.getenv('HOME')+"/.dlogs/.Schedule/"
-    now_dt = datetime.datetime.now()
-    date = (
-        datetime.datetime(
-            year=now_dt.year, month=now_dt.month, day=now_dt.day + day_num
-        )
-    )
-
-    def_date = str(round(int(date.strftime("%Y")),  -1)) + "s/" + date.strftime("%Y") + "/" + date.strftime("%b")
-    if not os.path.exists(logs_dir+def_date):
-        os.makedirs(logs_dir+def_date)
-    sch = logs_dir+def_date + "/" + date.strftime('%d-%m-%Y')
-    with open(f"{sch}.md", 'w') as f:
+def write_md():
+    file = get_file()
+    with open(f"{file}.md", 'w') as f:
         f.write("| Time | Name |\n")
         f.write("| :---: | :--: |")
-    events_db = init_db()
+    events_db = get_events()
     for event in events_db:
         name = event.get("name")
         if name is None:
-            name="UNTITLED"
+            name = "UNTITLED"
         category = event.get("category")
         if category is None:
             category = "REGULAR"
-        with open(f"{sch}.md", 'a') as f:
+        with open(f"{file}.md", 'a') as f:
             f.write(f"\n| {event.get('start')} | {name} [{category}] |")
 
 
+def parse_md():
+    event_db = []
+    now_dt = datetime.datetime.now()
+    file = get_file() + ".md"
+    if not os.path.isfile(file) or os.stat(file).st_size == 0:
+        print_center_text(f"file {file} either does not exist or is empty")
+        sys.exit()
+    with open(file, 'r') as buffer:
+        lines = buffer.readlines()
+        if len(lines) < 3:
+            print_center_text(f"file {file} only contains less than 3 lines as read function would not work the system is exiting")
+            sys.exit()
+        elif len(lines) == 3:
+            line = lines[2]
+            columns = line.split(' | ')
+            start_time = (datetime.datetime.strptime(columns[0].replace('| ', ''), "%H:%M:%S").replace(year=now_dt.year, day=now_dt.day + day_num, month=now_dt.month) + dGMT).isoformat()
+            name = columns[1].split('[')[0]
+            label = columns[1].split('[')[1].replace('] |\n', '')
+            end_time = (datetime.datetime(
+                year=now_dt.year, month=now_dt.month, day=now_dt.day + day_num + 1
+            ) + dGMT).isoformat()
+            event = {
+                    'name': name,
+                    'label': label,
+                    'start_time': start_time,
+                    'end_time': end_time
+                    }
+            event_db.append(event)
+        else:
+            lines.pop(0)
+            lines.pop(0)
+            for lc in range(0, len(lines)-1):
+                line = lines[lc]
+                columns = line.split(' | ')
+                start_time = (datetime.datetime.strptime(columns[0].replace('| ', ''), "%H:%M:%S").replace(year=now_dt.year, day=now_dt.day + day_num, month=now_dt.month) + dGMT).isoformat()
+                name = columns[1].split('[')[0]
+                label = columns[1].split('[')[1].replace('] |\n', '')
+                end_time = (datetime.datetime.strptime(lines[lc+1].split(' | ')[0].replace('| ', ''), "%H:%M:%S").replace(year=now_dt.year, day=now_dt.day + day_num, month=now_dt.month) + dGMT).isoformat()
+                event = {
+                        'name': name,
+                        'label': label,
+                        'start_time': start_time,
+                        'end_time': end_time
+                        }
+                event_db.append(event)
+            line = lines[-1]
+            columns = line.split(' | ')
+            start_time = (datetime.datetime.strptime(columns[0].replace('| ', ''), "%H:%M:%S").replace(year=now_dt.year, day=now_dt.day + day_num, month=now_dt.month) + dGMT).isoformat()
+            name = columns[1].split('[')[0]
+            label = columns[1].split('[')[1].replace('] |\n', '')
+            end_time = (datetime.datetime(
+                year=now_dt.year, month=now_dt.month, day=now_dt.day + day_num + 1
+            ) + dGMT).isoformat()
+            event = {
+                    'name': name,
+                    'label': label,
+                    'start_time': start_time,
+                    'end_time': end_time
+                    }
+            event_db.append(event)
+        return event_db
+
+
+def upload_md():
+    print("Reading file ...")
+    event_db = parse_md()
+    for data in event_db:
+        event = {
+                'summary': data['name'],
+                'start': {
+                    'dateTime': f"{data['start_time']}Z",
+                    'TimeZone': TIMEZONE
+                    },
+                'end': {
+                    'dateTime': f"{data['end_time']}Z",
+                    'TimeZone': TIMEZONE
+                    }
+                }
+        print("Uploading events")
+        service = init_api()
+        if data['label'] == "REGULAR":
+            cal_ids['REGULAR'] = 'primary'
+        event = service.events().insert(calendarId=cal_ids[data['label']], body=event).execute()
+        print(f"Event created at {event.get('htmlLink')}")
+
+
 if __name__ == "__main__":
-    print("getting events.....")
-    events_db = init_db()
-    for event in events_db:
-        name = event.get("name")
-        if name is None:
-            name="UNTITLED"
-        category = event.get("category")
-        if category is None:
-            category = "REGULAR"
-        print(event.get("start"))
-        print(f"  | {name} [{category}]")
-        print(event.get("end"))
-        print()
-    if is_md:
-        print("saving output to md")
-        md()
+    if not is_upload:
+        print_center_text("getting events.....")
+        output = ""
+        events_db = get_events()
+        for event in events_db:
+            name = event.get("name")
+            if name is None:
+                name = "UNTITLED"
+            category = event.get("category")
+            if category is None:
+                category = "REGULAR"
+            output += event.get("start") 
+            output += f"  | {name} [{category}]" + " | "
+            output += event.get("end") + "\n"
+        print_center_text(output)
+        if is_md:
+            print(f"SAVING OUTPUT TO MD FILE {get_file()}")
+            write_md()
+    else:
+        upload_md()
